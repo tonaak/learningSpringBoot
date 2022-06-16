569931b281dcd6eebc35395dc240db298094b5e5
tonaak <toannguyenitit@gmail.com>
2022-06-16 10:32:53 +0700

    Listing users

--- src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -14,8 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,6 +26,7 @@ import com.hoaxify.hoaxify.error.ApiError;
 import com.hoaxify.hoaxify.shared.GenericResponse;
 import com.hoaxify.hoaxify.user.User;
 import com.hoaxify.hoaxify.user.UserRepository;
+import com.hoaxify.hoaxify.user.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -36,6 +40,9 @@ public class UserControllerTest {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	UserService userService;
 
 	@Before
 	public void cleanup() {
@@ -49,6 +56,7 @@ public class UserControllerTest {
 		ResponseEntity<Object> response = postSignup(user, Object.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
+
 
 	@Test
 	public void postUser_whenUserIsValid_userSavedToDatabase() {
@@ -124,7 +132,7 @@ public class UserControllerTest {
 	@Test
 	public void postUser_whenUserHasUsernameExceedsTheLengthLimit_receiveBadRequest() {
 		User user = TestUtil.createValidUser();
-		String valueOf256Chars = IntStream.rangeClosed(1, 256).mapToObj(x -> "a").collect(Collectors.joining());
+		String valueOf256Chars = IntStream.rangeClosed(1,256).mapToObj(x -> "a").collect(Collectors.joining());
 		user.setUsername(valueOf256Chars);
 		ResponseEntity<Object> response = postSignup(user, Object.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -133,7 +141,7 @@ public class UserControllerTest {
 	@Test
 	public void postUser_whenUserHasDisplayNameExceedsTheLengthLimit_receiveBadRequest() {
 		User user = TestUtil.createValidUser();
-		String valueOf256Chars = IntStream.rangeClosed(1, 256).mapToObj(x -> "a").collect(Collectors.joining());
+		String valueOf256Chars = IntStream.rangeClosed(1,256).mapToObj(x -> "a").collect(Collectors.joining());
 		user.setDisplayName(valueOf256Chars);
 		ResponseEntity<Object> response = postSignup(user, Object.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -142,7 +150,7 @@ public class UserControllerTest {
 	@Test
 	public void postUser_whenUserHasPasswordExceedsTheLengthLimit_receiveBadRequest() {
 		User user = TestUtil.createValidUser();
-		String valueOf256Chars = IntStream.rangeClosed(1, 256).mapToObj(x -> "a").collect(Collectors.joining());
+		String valueOf256Chars = IntStream.rangeClosed(1,256).mapToObj(x -> "a").collect(Collectors.joining());
 		user.setPassword(valueOf256Chars + "A1");
 		ResponseEntity<Object> response = postSignup(user, Object.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -219,8 +227,7 @@ public class UserControllerTest {
 		user.setPassword("alllowercase");
 		ResponseEntity<ApiError> response = postSignup(user, ApiError.class);
 		Map<String, String> validationErrors = response.getBody().getValidationErrors();
-		assertThat(validationErrors.get("password"))
-				.isEqualTo("Password must have at least one uppercase, one lowercase letter and one number");
+		assertThat(validationErrors.get("password")).isEqualTo("Password must have at least one uppercase, one lowercase letter and one number");
 	}
 
 	@Test
@@ -242,8 +249,96 @@ public class UserControllerTest {
 		assertThat(validationErrors.get("username")).isEqualTo("This name is in use");
 	}
 
-	public <T> ResponseEntity<T> postSignup(Object request, Class<T> response) {
+	@Test
+	public void getUsers_whenThereAreNoUsersInDB_receiveOK() {
+		ResponseEntity<Object> response = getUsers(new ParameterizedTypeReference<Object>() {});
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	public void getUsers_whenThereAreNoUsersInDB_receivePageWithZeroItems() {
+		ResponseEntity<TestPage<Object>> response = getUsers(new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getTotalElements()).isEqualTo(0);
+	}	
+
+	@Test
+	public void getUsers_whenThereIsAUserInDB_receivePageWithUser() {
+		userRepository.save(TestUtil.createValidUser());
+		ResponseEntity<TestPage<Object>> response = getUsers(new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getNumberOfElements()).isEqualTo(1);
+	}
+	
+	@Test
+	public void getUsers_whenThereIsAUserInDB_receiveUserWithoutPassword() {
+		userRepository.save(TestUtil.createValidUser());
+		ResponseEntity<TestPage<Map<String, Object>>> response = getUsers(new ParameterizedTypeReference<TestPage<Map<String, Object>>>() {});
+		Map<String, Object> entity = response.getBody().getContent().get(0);
+		assertThat(entity.containsKey("password")).isFalse();
+	}
+	
+	@Test
+	public void getUsers_whenPageIsRequestedFor3ItemsPerPageWhereTheDatabaseHas20Users_receive3Users() {
+		IntStream.rangeClosed(1, 20).mapToObj(i -> "test-user-"+i)
+			.map(TestUtil::createValidUser)
+			.forEach(userRepository::save);
+		String path = API_1_0_USERS + "?page=0&size=3";
+		ResponseEntity<TestPage<Object>> response = getUsers(path, new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getContent().size()).isEqualTo(3);
+	}
+
+	@Test
+	public void getUsers_whenPageSizeNotProvided_receivePageSizeAs10() {
+		ResponseEntity<TestPage<Object>> response = getUsers(new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getSize()).isEqualTo(10);
+	}	
+
+	@Test
+	public void getUsers_whenPageSizeIsGreaterThan100_receivePageSizeAs100() {
+		String path = API_1_0_USERS + "?size=500";
+		ResponseEntity<TestPage<Object>> response = getUsers(path, new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getSize()).isEqualTo(100);
+	}	
+
+	@Test
+	public void getUsers_whenPageSizeIsNegative_receivePageSizeAs10() {
+		String path = API_1_0_USERS + "?size=-5";
+		ResponseEntity<TestPage<Object>> response = getUsers(path, new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getSize()).isEqualTo(10);
+	}
+
+	@Test
+	public void getUsers_whenPageIsNegative_receiveFirstPage() {
+		String path = API_1_0_USERS + "?page=-5";
+		ResponseEntity<TestPage<Object>> response = getUsers(path, new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getNumber()).isEqualTo(0);
+	}
+	
+	@Test
+	public void getUsers_whenUserLoggedIn_receivePageWithouLoggedInUser() {
+		userService.save(TestUtil.createValidUser("user1"));
+		userService.save(TestUtil.createValidUser("user2"));
+		userService.save(TestUtil.createValidUser("user3"));
+		authenticate("user1");
+		ResponseEntity<TestPage<Object>> response = getUsers(new ParameterizedTypeReference<TestPage<Object>>() {});
+		assertThat(response.getBody().getTotalElements()).isEqualTo(2);
+	}
+
+
+	private void authenticate(String username) {
+		testRestTemplate.getRestTemplate()
+			.getInterceptors().add(new BasicAuthenticationInterceptor(username, "P4ssword"));
+	}
+
+	public <T> ResponseEntity<T> postSignup(Object request, Class<T> response){
 		return testRestTemplate.postForEntity(API_1_0_USERS, request, response);
+	}
+
+	public <T> ResponseEntity<T> getUsers(ParameterizedTypeReference<T> responseType){
+		return testRestTemplate.exchange(API_1_0_USERS, HttpMethod.GET, null, responseType);
+	}
+	
+	public <T> ResponseEntity<T> getUsers(String path, ParameterizedTypeReference<T> responseType){
+		return testRestTemplate.exchange(path, HttpMethod.GET, null, responseType);
 	}
 
 }
