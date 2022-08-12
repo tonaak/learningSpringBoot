133358a32c9eefd56f009d9513d470d4254a9836
tonaak <toannguyenitit@gmail.com>
2022-08-12 14:18:06 +0700

    config

--- src/main/java/com/troke/troke/user/User.java
+++ src/main/java/com/troke/troke/user/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -24,6 +25,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "\"User\"")
 public class User implements UserDetails {
 
 	/**
